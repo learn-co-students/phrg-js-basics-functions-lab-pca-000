51abc35e3f22cbc89808b1ebcffe3eb3f53f727f
Daniel Mack <daniel.mack@powerhrg.com>
2018-08-13 18:30:52 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+      return block - 42
+  } else {
+    return 42 - block
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(blockStart, blockEnd) {
+  if (blockStart < blockEnd) {
+    return (blockEnd - blockStart) * 264
+  } else {
+    return (blockStart - blockEnd) * 264
+  }
+}
+
+function calculatesFarePrice(blockStart, blockEnd) {
+  const travel = distanceTravelledInFeet(blockStart, blockEnd)
+  if (travel <= 400) {
+    return 0
+  } else if (travel <= 2000) {
+    return 2.56
+  } else if (travel <= 2499) {
+    return 25
+  } else {
+    return "cannot travel that far"
+  }
+}
