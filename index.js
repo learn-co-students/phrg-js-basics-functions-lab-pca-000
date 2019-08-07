b83e1f1b0172b7852d2b9dd6e123ab9f2c0be08a
Gillian Wenhold <gwenhold@mac.com>
2019-08-07 13:07:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+  const distanceInBlocks = Math.abs(42 - destination);
+  return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(destination) {
+  const distanceInFeet = distanceFromHqInBlocks(destination) * 264;
+  return distanceInFeet;
+}
+
+function distanceTravelledInFeet(beginning, end) {
+  const distanceTravelled = Math.abs(end - beginning) * 264;
+  return distanceTravelled;
+}
+
+function calculatesFarePrice(beginning, end) {
+  const distance = distanceTravelledInFeet(beginning, end);
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return (distance - 400) * .02;
+  } else {
+    return 0;
+  }
+}
